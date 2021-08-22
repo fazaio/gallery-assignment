5b7ea26adfc37029698aba0eea4d37e1e21277fa
fazaio <faza6028@gmail.com>
2021-08-23 00:41:33 +0700

    init middleware

--- server/app.js
+++ server/app.js
@@ -2,12 +2,13 @@ import express from "express";
 import router from './src/routes/'
 
 const app = express();
+const auth = require('./src/middlewares/auth')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/data', router.data)
-app.use('/admin', router.admin)
+app.use('/admin', auth , router.admin)
 
 app.listen(5000,() => {
      console.log(`app is listening to port 5000`);
